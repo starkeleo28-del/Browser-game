b4458cf34c68d5e7ce0f36e161086db870859f75
starkeleo28-del <starkeleo28@gmail.com>
2026-09-15 15:45:26 +0200

    brower

---
+++ logic.js
@@ -0,0 +1,13 @@
+let point = document.querySelector("point")
+let Button = document.querySelector("Button")
+let input = document.querySelector("input")
+
+
+const image = document.querySelector("#clickImage");
+
+image.addEventListener("click", function () {
+    console.log("Bilden klickades!");
+    if (#clickImage, )
+});
+
+  
