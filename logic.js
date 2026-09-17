f91df0aed004133897fb86553d5d9eee3a30df09
starkeleo28-del <starkeleo28@gmail.com>
2026-09-17 11:55:11 +0200

    browser game

--- logic.js
+++ logic.js
@@ -1,13 +1,28 @@
-let point = document.querySelector("point")
-let Button = document.querySelector("Button")
-let input = document.querySelector("input")
+const image = document.querySelector("#click");
+const point = document.querySelector("#score");
+const pointupgrade = document.querySelector("#click");
+const pointperclick = document.querySelector("#click");
+
+let upgradepointsperclickcost = 10*2
+let upgradeclicksperseccost = 8*2
+let pointperupgrade = 1*2
 
 
-const image = document.querySelector("#clickImage");
+image.addEventListener("click", function(point) {
+    console.log ("image clicked!")
+    
 
-image.addEventListener("click", function () {
-    console.log("Bilden klickades!");
-    if (#clickImage, )
-});
+})
+if ("click", point === 1){}
 
+
+
+
+
+
+
+
+
+
+  
   
