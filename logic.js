7a1f88aae2cba01266bb089d789fb8187ced3b5a
starkeleo28-del <starkeleo28@gmail.com>
2026-09-17 15:45:16 +0200

    smartschoolboynein clicker
    Jag har gjort ett clicker spel där man kan upgradera hur mycket poäng man får per click. Men spelet är inte färdigt än och behöver finslippning.

--- logic.js
+++ logic.js
@@ -1,17 +1,20 @@
 const image = document.querySelector("#click");
 const point = document.querySelector("#points");
 const pointUpgrades = document.querySelector("#pointUpgrade");
-const pointperclicktext = document.querySelector("#pointperclick");
+const pointPerClickText = document.querySelector("#pointsPerClick");
+const upgradeCostText = document.querySelector("#upgradecost");
+const upgradeCostBar = document.querySelector("#upgradeCostBar");
 
 let upgradepointperclickcost = 10*2
 let upgradeclickperseccost = 8*2
-let pointperupgrade = 1*2
-let upgradepointperclick = 1*2
+let pointperupgrade = 1
+let upgradepointperclick = 1+1
 let points = 0
+let upgradecost = 20*2
 
 image.addEventListener("click", function() {
     console.log ("image clicked!")
-    points += 1
+    points += pointperupgrade
     point.textContent = points;
     document.querySelector("click")
 document.querySelector("#click")
@@ -20,9 +23,31 @@ document.querySelector("#click")
 {
     pointUpgrades.addEventListener("click", function() {
         console.log ("upgrade clicked!")
-        pointperupgrade
         point.textContent = points;
         document.querySelector("click")
         document.querySelector("#click")
 })
+}
+{
+    pointUpgrades.addEventListener("click", function(){
+        document.querySelector("#pointsPerClick")
+    if (points >= upgradepointperclickcost){
+        pointperupgrade += 1
+        upgradepointperclickcost = 
+        Math.ceil(upgradepointperclickcost * 1.5);
+        point.textContent = points;
+        pointPerClickText.textContent = pointperupgrade;
+
+    }
+});
+}
+{
+    console.log(upgradeCostText);
+    upgradepointperclickcost = Math.ceil(upgradepointperclickcost * 1.5);
+upgradeCostText.textContent = upgradepointperclickcost; 
+upgradeCostBar.max = upgradepointperclickcost;
+upgradeCostBar.value = points;
+
+
+
 }
